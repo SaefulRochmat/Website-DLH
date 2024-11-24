d91faaf9d6202ca88fd4f17bfc8a071573e60a94
unknown <apipska.10@gmail.com>
2024-11-24 18:39:25 +0700

    Adding Dynamic at Kontak

--- app/kontak/page.js
+++ app/kontak/page.js
@@ -1,18 +1,23 @@
+"use client";
 import dynamic from "next/dynamic";
 import Footer from "../components/footer";
 import NavBarWithOutEffect from "../components/NavBarWithOutEffect";
 
-export async function generateMetadata() {
-	return {
-		title: "Kontak | Dinas Lingkungan Hidup Majalengka",
-		description:
-			"Informasi Kontak Dinas Lingkungan Hidup di Kabupaten Majalengka.",
-	};
-}
+// export async function generateMetadata() {
+// 	return {
+// 		title: "Kontak | Dinas Lingkungan Hidup Majalengka",
+// 		description:
+// 			"Informasi Kontak Dinas Lingkungan Hidup di Kabupaten Majalengka.",
+// 	};
+// }
 
+// const Map = dynamic(() => import("@/components/map"), {
+// 	ssr: false,
+// });
 const ContactPage = dynamic(() => import("../components/KontakPage"), {
 	ssr: false,
 });
+
 export default function HalamanKontak() {
 	return (
 		<>
