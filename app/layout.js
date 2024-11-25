be386a31fe778ca3568f07aabc6cfd0aacb7679b
ASEP-PC <saeful.rochmat240302@gmail.com>
2024-11-25 19:11:24 +0700

    Menambahkan Speed Insight untuk vercel di layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import localFont from "next/font/local";
 import "./globals.css";
 import 'leaflet/dist/leaflet.css';
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
